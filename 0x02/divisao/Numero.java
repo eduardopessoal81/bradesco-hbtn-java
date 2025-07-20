501456a578239c4a274b8ca180202a4c258142ac
eduardopessoal81 <eduardopessoal@hotmail.com>
2025-07-19 22:35:56 -0300

    Divisao

---
+++ 0x02/divisao/Numero.java
@@ -0,0 +1,19 @@
+public class Numero {
+
+    public static void dividir(int a, int b) {
+        int resultado = 0;
+        boolean sucesso = true;
+
+        try {
+            resultado = a / b;
+        } catch (ArithmeticException e) {
+            sucesso = false;
+        } finally {
+            if (sucesso) {
+                System.out.println(a + " / " + b + " = " + resultado);
+            } else {
+                System.out.println("Nao eh possivel dividir por zero");
+            }
+        }
+    }
+}
