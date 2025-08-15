e81e014ac24a6258acd482bd5057d5cebde04a6e
eduardopessoal81 <eduardopessoal@hotmail.com>
2025-08-15 12:35:30 -0300

    Lendo Arquivo CSV

--- arquivos/2/CsvFileReader.java
+++ arquivos/2/CsvFileReader.java
@@ -8,19 +8,23 @@ public class CsvFileReader {
 
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String linha;
-            reader.readLine();
 
             while ((linha = reader.readLine()) != null) {
                 if (linha.trim().isEmpty()) {
                     continue;
                 }
 
-                String[] dados = linha.split(",");
+                String[] dados = linha.split(",", -1);
 
-                System.out.println("Funcionário: " + dados[0]);
-                System.out.println("Idade: " + dados[1]);
-                System.out.println("Departamento: " + dados[2]);
-                System.out.println("Salarial: " + dados[3]);
+                String nome = dados.length > 0 ? dados[0] : "";
+                String idade = dados.length > 1 ? dados[1] : "";
+                String departamento = dados.length > 2 ? dados[2] : "";
+                String salarial = dados.length > 3 ? dados[3] : "";
+
+                System.out.println("Funcionário: " + nome);
+                System.out.println("Idade: " + idade);
+                System.out.println("Departamento: " + departamento);
+                System.out.println("Salarial: " + salarial);
                 System.out.println("------------------------");
             }
 
