bf029e881acc308ba129ff2ecdb7172bdd5834bf
eduardodesenvolvedor <eduardofreire.contato@gmail.com>
2025-09-06 01:18:07 -0300

    Blog 2

---
+++ Collections_Exercicios/postagem_blog/Autor.java
@@ -0,0 +1,42 @@
+public class Autor implements Comparable<Autor> {
+    private String nome;
+    private String sobrenome;
+
+    public Autor(String nome, String sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " " + sobrenome;
+    }
+
+    @Override
+    public int compareTo(Autor outro) {
+        int sobrenomeCompare = this.sobrenome.compareTo(outro.sobrenome);
+        if (sobrenomeCompare != 0) return sobrenomeCompare;
+        return this.nome.compareTo(outro.nome);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Autor)) return false;
+        Autor outro = (Autor) obj;
+        return nome.equals(outro.nome) && sobrenome.equals(outro.sobrenome);
+    }
+
+    @Override
+    public int hashCode() {
+        return nome.hashCode() + 31 * sobrenome.hashCode();
+    }
+}
---
+++ Collections_Exercicios/postagem_blog/Blog.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class Blog {
+    private List<Post> postagens;
+
+    public Blog() {
+        this.postagens = new ArrayList<>();
+    }
+
+    public void adicionarPostagem(Post post) {
+        if (postagens.contains(post)) {
+            throw new IllegalArgumentException("Postagem jah existente");
+        }
+        postagens.add(post);
+    }
+
+    public Set<Autor> obterTodosAutores() {
+        TreeSet<Autor> autores = new TreeSet<>();
+        for (Post p : postagens) {
+            autores.add(p.getAutor());
+        }
+        return autores;
+    }
+
+    public Map<Categorias, Integer> obterContagemPorCategoria() {
+        Map<Categorias, Integer> contagem = new HashMap<>();
+        for (Post p : postagens) {
+            contagem.put(p.getCategoria(), contagem.getOrDefault(p.getCategoria(), 0) + 1);
+        }
+        return contagem;
+    }
+
+    public Set<Post> obterPostsPorAutor(Autor autor) {
+        TreeSet<Post> resultados = new TreeSet<>();
+        for (Post p : postagens) {
+            if (p.getAutor().equals(autor)) {
+                resultados.add(p);
+            }
+        }
+        return resultados;
+    }
+
+    public Set<Post> obterPostsPorCategoria(Categorias categoria) {
+        TreeSet<Post> resultados = new TreeSet<>();
+        for (Post p : postagens) {
+            if (p.getCategoria() == categoria) {
+                resultados.add(p);
+            }
+        }
+        return resultados;
+    }
+
+    public Map<Categorias, Set<Post>> obterTodosPostsPorCategorias() {
+        Map<Categorias, Set<Post>> map = new HashMap<>();
+        for (Post p : postagens) {
+            map.computeIfAbsent(p.getCategoria(), k -> new TreeSet<>()).add(p);
+        }
+        return map;
+    }
+
+    public Map<Autor, Set<Post>> obterTodosPostsPorAutor() {
+        Map<Autor, Set<Post>> map = new HashMap<>();
+        for (Post p : postagens) {
+            map.computeIfAbsent(p.getAutor(), k -> new TreeSet<>()).add(p);
+        }
+        return map;
+    }
+}
