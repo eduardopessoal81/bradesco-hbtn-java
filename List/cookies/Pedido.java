7031ac84c6076511147eb5494f513c9ceb24370e
eduardodesenvolvedor <eduardofreire.contato@gmail.com>
2025-09-06 00:55:10 -0300

    Pedido Cookie

--- List/cookies/Pedido.java
+++ List/cookies/Pedido.java
@@ -1,9 +1,8 @@
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
 public class Pedido {
-    private List<PedidoCookie> cookies;
+    private ArrayList<PedidoCookie> cookies;
 
     public Pedido() {
         this.cookies = new ArrayList<>();
