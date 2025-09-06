d62159ce0dffd393c40e3b7c2cb3ce2ceec784ce
eduardodesenvolvedor <eduardofreire.contato@gmail.com>
2025-09-06 00:50:48 -0300

    Pedido Cookie

---
+++ List/cookies/Pedido.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Pedido {
+    private List<PedidoCookie> cookies;
+
+    public Pedido() {
+        this.cookies = new ArrayList<>();
+    }
+
+    public void adicionarPedidoCookie(PedidoCookie pedidoCookie) {
+        cookies.add(pedidoCookie);
+    }
+
+    public int obterTotalCaixas() {
+        int total = 0;
+        for (PedidoCookie pc : cookies) {
+            total += pc.getQuantidadeCaixas();
+        }
+        return total;
+    }
+
+    public int removerSabor(String sabor) {
+        int removidos = 0;
+        Iterator<PedidoCookie> it = cookies.iterator();
+        while (it.hasNext()) {
+            PedidoCookie pc = it.next();
+            if (pc.getSabor().equalsIgnoreCase(sabor)) {
+                removidos += pc.getQuantidadeCaixas();
+                it.remove();
+            }
+        }
+        return removidos;
+    }
+}
