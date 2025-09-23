c184684f526bd56525de1815bf4195f73698d442
eduardodesenvolvedor <eduardofreire.contato@gmail.com>
2025-09-23 19:21:37 -0300

    Preco Minimo

---
+++ Streams_Collectors_Optional/preco_minimo/Consulta.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+public class Consulta {
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos.stream()
+                .max(Comparator.comparing(Produto::getPreco))
+                .orElse(null);
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double precoMinimo) {
+        return produtos.stream()
+                .filter(p -> p.getPreco() >= precoMinimo)
+                .collect(Collectors.toList());
+    }
+}
