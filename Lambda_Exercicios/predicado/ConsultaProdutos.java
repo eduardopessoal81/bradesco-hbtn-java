7ebe83c67c5e73ea7bc1dd4939228aa04ceb6187
eduardodesenvolvedor <eduardofreire.contato@gmail.com>
2025-09-23 08:37:27 -0300

    Predicado

---
+++ Lambda_Exercicios/predicado/ConsultaProdutos.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ConsultaProdutos {
+
+    public static List<Produto> filtrar(List<Produto> produtos, Predicate<Produto> criterio) {
+        List<Produto> resultado = new ArrayList<>();
+        for (Produto p : produtos) {
+            if (criterio.test(p)) {
+                resultado.add(p);
+            }
+        }
+        return resultado;
+    }
+}
---
+++ Lambda_Exercicios/predicado/Produto.java
@@ -0,0 +1,47 @@
+import java.util.Locale;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private double peso;
+    private int quantidadeEmEstoque;
+    private TiposProduto tipo;
+
+    public Produto(String nome, double preco, double peso, int quantidadeEmEstoque, TiposProduto tipo) {
+        this.nome = nome;
+        this.preco = preco;
+        this.peso = peso;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public int getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public TiposProduto getTipo() {
+        return tipo;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %s %d %s",
+                nome,
+                String.format(Locale.getDefault(), "%.6f", preco),
+                String.format(Locale.getDefault(), "%.6f", peso),
+                quantidadeEmEstoque,
+                tipo);
+    }
+}
