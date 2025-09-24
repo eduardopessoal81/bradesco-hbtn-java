b57ee0a98df2a0a2b6b25dc6da2a018f1d14c830
eduardodesenvolvedor <eduardofreire.contato@gmail.com>
2025-09-23 22:05:12 -0300

    Hobbies Cargo

---
+++ Streams_Collector_Optional_Exercicios/hobbies_por_cargo/Pessoa.java
@@ -0,0 +1,90 @@
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.List;
+import java.util.Locale;
+
+public class Pessoa implements Comparable<Pessoa> {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+    private List<String> hobbies;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario, List<String> hobbies) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+        this.hobbies = hobbies;
+    }
+
+	@Override
+	public String toString() {
+		DecimalFormatSymbols symbols = new DecimalFormatSymbols(new Locale("pt", "BR"));
+		DecimalFormat df = new DecimalFormat("0.000000", symbols);
+		return String.format("[%d] %s %s %d R$ %s", codigo, nome, cargo, idade, df.format(salario));
+	}
+
+    @Override
+    public int compareTo(Pessoa outra) {
+        int nomeComparison = this.nome.compareTo(outra.nome);
+        if (nomeComparison != 0) {
+            return nomeComparison;
+        }
+        int idadeComparison = Integer.compare(this.idade, outra.idade);
+        if (idadeComparison != 0) {
+            return idadeComparison;
+        }
+        return Double.compare(this.salario, outra.salario);
+    }
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public List<String> getHobbies() {
+		return hobbies;
+	}
+
+	public void setHobbies(List<String> hobbies) {
+		this.hobbies = hobbies;
+	}
+}
